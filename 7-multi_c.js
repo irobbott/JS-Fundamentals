1a1b10195c72d1404e316cd87f21b1286150a25f
Iconic Robbott <dev.robbott@gmail.com>
2025-11-08 08:03:59 +0100

    7. I love C

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+const x = parseInt(process.argv[2]);
+
+if (isNaN(x)) {
+  console.log("Missing number of occurrences");
+} else {
+  let i = 0;
+  let output = "";
+  while (i < x) {
+    output += "C is fun\n";
+    i++;
+  }
+  console.log(output.slice(0, -1)); // Remove last newline
+}
